8ff00f4b141c02383c8325479fac14f070fc5443
Brian Griffin <wbgriffin@pm.me>
2023-07-05 23:27:14 -0400

    feat: add reset and layout styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 function App() {
 
   return (
-    <main>
-      <h1>Welcome</h1>
-      <p>This is some body text.</p>
+    <main className="container">
+      <p>header</p>
+      <p>search</p>
+      <p>main area</p>
+      <p>😕</p>
+      <h2>No Definitions Found</h2>
+      <p>Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at later time or head to the web instead.</p>
     </main>
   )
 }
