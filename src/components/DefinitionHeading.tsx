9889f2243061fdcae0b281f51b5b898bfe902843
Brian Griffin <wbgriffin@pm.me>
2023-07-07 11:43:44 -0400

    feat: add phoenetic audio

--- src/components/DefinitionHeading.tsx
+++ src/components/DefinitionHeading.tsx
@@ -1,5 +1,6 @@
+import { useRef } from 'react'
 import { Phonetic } from '../api/dictionary'
-import playButton from '../assets/play.svg'
+import playIcon from '../assets/play.svg'
 
 function DefinitionHeading({
   word,
@@ -8,11 +9,23 @@ function DefinitionHeading({
   word: string
   phoenetic: Phonetic
 }) {
+
+  const audioRef = useRef<HTMLAudioElement>(null)
+
   return (
     <div className="DefinitionHeading">
       <h1 className="DefinitionHeading__word">{word}</h1>
       <span className="DefinitionHeading__phoenetic">{phoenetic.text}</span>
-      <img src={playButton} className="DefinitionHeading__play" />
+      {phoenetic.audio && (
+        <>
+          <audio src={phoenetic.audio} ref={audioRef} />
+          <img
+            src={playIcon}
+            className="DefinitionHeading__play"
+            onClick={() => audioRef.current?.play()}
+          />
+        </>
+      )}
     </div>
   )
 }
