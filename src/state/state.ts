c70ad67ef3fbc1ad7aeb3034de47bc2533ec86e6
Brian Griffin <wbgriffin@pm.me>
2023-07-17 10:09:10 -0400

    feat: initialize theme to browser preference

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,5 +1,6 @@
 import { DictionaryResult } from '../api/dictionary'
 import { Font, Theme } from '../types'
+import { prefersDarkTheme } from '../helpers'
 
 export interface AppState {
   font: Font
@@ -10,7 +11,7 @@ export interface AppState {
 
 export const initialAppState: AppState = {
   font: Font.SansSerif,
-  theme: Theme.Light,
+  theme: prefersDarkTheme() ? Theme.Dark : Theme.Light,
   searchTerm: '',
   definition: null,
 }
